6a0930128e2ab0ec728063670b062e0927b2e347
augusto.otto <augusto.otto@mercadolivre.com>
2023-06-03 15:29:13 -0300

    feat: add auth logic

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,14 +1,35 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { sendLoginEmail, login, onAuthChanged, logout } from '@/services/auth';
 
 const Page = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState(
+    window.localStorage.getItem('emailForSignIn') || ''
+  );
+  const [user, setUser] = useState(false);
+
+  useEffect(() => {
+    const unsub = onAuthChanged((user) => setUser(!!user));
+
+    return unsub;
+  }, []);
+
+  useEffect(() => {
+    login(email);
+  }, [email]);
+
   return (
     <section>
-      <Input value={email} onChange={(e) => setEmail(e.target.value)} />
-      <Button>Login</Button>
+      {user ? (
+        <Button onClick={logout}>Logout</Button>
+      ) : (
+        <>
+          <Input value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Button onClick={() => sendLoginEmail(email)}>Login</Button>
+        </>
+      )}
     </section>
   );
 };
--- services/auth.ts
+++ services/auth.ts
@@ -5,6 +5,8 @@ import {
   isSignInWithEmailLink,
   signInWithEmailLink,
   User,
+  onAuthStateChanged,
+  signOut,
 } from 'firebase/auth';
 
 const actionCodeSettings: ActionCodeSettings = {
@@ -19,11 +21,19 @@ export const sendLoginEmail = async (email: string) => {
 };
 
 export const login = async (email: string): Promise<User | void> => {
-  if (!isSignInWithEmailLink(auth, window.location.href)) return;
+  if (!isSignInWithEmailLink(auth, window.location.href) || !email) return;
 
   const { user } = await signInWithEmailLink(auth, email, window.location.href);
 
   window.localStorage.removeItem('emailForSignIn');
 
   return user;
+};
+
+export const onAuthChanged = (callback: (user: User | null) => void) => {
+  return onAuthStateChanged(auth, callback);
+};
+
+export const logout = () => {
+  signOut(auth);
 };
